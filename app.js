b55d0bf585fd1aa52e90293ac5f463d0735366f9
lukephelan <luke.phelan@hotmail.com>
2016-08-06 15:40:11 +1000

    Fixed typo

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/json', function (req, res){
         // Retrieve all relationships from the database
         db.cypherQuery("START n=node(*) MATCH (n)-[r]->(m) RETURN {source: id(n), target: id(m), value: r.value, name: r.name}", function(err, result){
             if(err) throw err;
-            object.links = result.data
+            object.links = result.data;
             // Send the object as a response
             res.send(object);
         });
